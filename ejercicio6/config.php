1122b4997088b3a2eeaa745f0f1401a14f17cc40
joelVerdugo198 <69660323+joelVerdugo198@users.noreply.github.com>
2020-10-13 12:59:50 -0600

    Add files via upload

---
+++ ejercicio6/config.php
@@ -0,0 +1,19 @@
+<?php
+
+	//mostrar error
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
+	//iniciar la sesion
+	if (!isset($_SESSION)) {
+		session_start();
+	}
+	
+	//crear token
+	if (!isset($_SESSION['token'])) {
+		$_SESSION['token'] = md5 (uniqid(mt_rand(),true) );
+		
+	}
+
+?>
---
+++ ejercicio6/userController.php
@@ -0,0 +1,204 @@
+<?php
+
+	include_once "config.php";
+	include_once "connection.php";
+
+	if (isset($_POST) && isset($_POST['action'])) {
+		if ($_POST['token'] == $_SESSION['token']) {			
+
+			$userController = new userController();
+
+			switch ($_POST['action']) {
+				case 'store':
+					$name = strip_tags($_POST['name']);
+					$email = strip_tags($_POST['email']);
+					$pass = strip_tags($_POST['password']);
+
+					$userController->store($name,$email,$pass);
+					
+				break;
+
+				case 'update':
+					$name = strip_tags($_POST['name']);
+					$email = strip_tags($_POST['email']);
+					$pass = strip_tags($_POST['password']);
+					$id = strip_tags($_POST['id']);
+
+					$userController->update($name,$email,$pass,$id);
+					
+				break;
+
+				case 'remove':
+					$id = strip_tags($_POST['user_id']);
+					echo json_encode($userController->remove($id));
+				break;
+			}
+		}else{
+			$respuesta = array(
+				'status' => "error",
+				'message' => "Sin autorización"
+			);
+
+			echo json_encode($respuesta);
+		}
+	}
+
+	Class userController
+	{
+		function get()
+		{
+			$conn = connect();
+			if (!$conn->connect_error) {
+				
+				$query = "select * from users";
+				$prepared_query = $conn->prepare($query);
+				$prepared_query->execute();
+
+				$results = $prepared_query->get_result();
+				$users = $results->fetch_all(MYSQLI_ASSOC);
+
+				if ($users) {
+					return $users;
+				}else{
+					return array();
+				}
+			}else{
+				return array();
+			}
+		}
+
+		public function store($name, $email, $password)
+		{
+			$conn = connect();
+			if (!$conn->connect_error) {
+
+				if ($name!="" && $email!="" && $password!="") {
+					$query = "insert into users (name,email,password) values (?,?,?)";
+					$prepared_query = $conn->prepare($query);
+					$prepared_query->bind_param('sss',$name,$email,$password);
+					if ($prepared_query->execute()) {
+
+						$_SESSION['status'] = "success";
+						$_SESSION['message'] = "El registro se ha guardado correctamente";
+
+						header('Location: '. $_SERVER['HTTP_REFERER']);
+					}else{
+
+						$_SESSION['status'] = "error";
+						$_SESSION['message'] = "El registro no ha guardado";
+
+						header('Location: '. $_SERVER['HTTP_REFERER']);
+					}
+
+				}else{
+
+						$_SESSION['status'] = "error";
+						$_SESSION['message'] = "Verifique la información enviada";
+
+				header('Location: '. $_SERVER['HTTP_REFERER']);
+				}
+			}else{
+
+				$_SESSION['status'] = "error";
+				$_SESSION['message'] = "Error durante la conexión";
+
+				header('Location: '. $_SERVER['HTTP_REFERER']);
+			}
+		}
+
+		public function update($name, $email, $password, $id)
+		{
+			$conn = connect();
+			if (!$conn->connect_error) {
+
+				if ($name!="" && $email!="" && $password!="") {
+					$query = "update users set name = ?,email = ?,password = ? where id = ?";
+					$prepared_query = $conn->prepare($query);
+					$prepared_query->bind_param('sssi', $name, $email, $password, $id);
+					if ($prepared_query->execute()) {
+
+						//var_dump($_POST);
+
+						$_SESSION['status'] = "success";
+						$_SESSION['message'] = "El registro se ha actualizado correctamente";
+
+						header('Location: '. $_SERVER['HTTP_REFERER']);
+					}else{
+
+						$_SESSION['status'] = "error";
+						$_SESSION['message'] = "El registro no ha actualizado";
+
+						header('Location: '. $_SERVER['HTTP_REFERER']);
+					}
+
+				}else{
+
+						$_SESSION['status'] = "error";
+						$_SESSION['message'] = "Verifique la información enviada";
+
+				header('Location: '. $_SERVER['HTTP_REFERER']);
+				}
+			}else{
+
+				$_SESSION['status'] = "error";
+				$_SESSION['message'] = "Error durante la conexión";
+
+				header('Location: '. $_SERVER['HTTP_REFERER']);
+			}
+		}
+
+		public function remove($id){
+
+			$conn = connect();
+			if (!$conn->connect_error) {
+
+				if ($id!="") {
+					$query = "delete from users where id = ?";
+					$prepared_query = $conn->prepare($query);
+					$prepared_query->bind_param('i', $id);
+					if ($prepared_query->execute()) {
+
+						//var_dump($_POST);
+
+						$respuesta = array(
+							'status' => "success",
+							'message' => "El registro se ha eliminado correctamente"
+						);
+
+						return $respuesta;
+					}else{
+
+						$respuesta = array(
+							'status' => "error",
+							'message' => "El registro se ha eliminado"
+						);
+
+						return $respuesta;
+					}
+
+				}else{
+
+						$respuesta = array(
+							'status' => "error",
+							'message' => "Verifique la información enviada"
+						);
+
+						return $respuesta;
+
+				}
+			}else{
+
+				$respuesta = array(
+							'status' => "error",
+							'message' => "Error durante la conexión"
+				);
+
+				return $respuesta;
+
+
+			}
+
+		}
+	}
+
+?>
